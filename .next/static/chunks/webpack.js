7b52ee12f7c7a3ee538df32d142a5cded71a2b4f
Berra Sarı <berrasari@icloud.com>
2022-07-20 13:53:33 +0300

    fvdfdf

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8239615fd3048a94"; }
+/******/ 		__webpack_require__.h = function() { return "39b79bdea0a888ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="flex justify-center">
+      <footer className="border-transparent text-gray-700 flex justify-center px-1 pt-1 border-b-2 text-sm font-medium">
         <div>Copyright 2022 Popup Builder </div>
       </footer>
     );
