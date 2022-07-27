30ab44d578cbf09783f9a7797400ca88bdb5db30
Berra Sarı <berrasari@icloud.com>
2022-07-27 12:41:03 +0300

    rtzrtz

--- components/Navbar_2.js
+++ components/Navbar_2.js
@@ -1,8 +1,14 @@
-/* This example requires Tailwind CSS v2.0+ */
 import Link from 'next/link'
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import Image from 'next/image'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+const items = [
+  { name: 'English', href: '/' },
+  { name: 'Español', href: '/espanol/' },
+
+]
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
@@ -51,13 +57,42 @@ export default function Navbar_2() {
                   >
                     Sobre nosotras
                   </Link></div>
-                  <div
-                    className="border-transparent text-gray-700 hover:border-indigo-500 hover:text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    ><Link
-                    href="/"
-                  >
-                    Switch : EN
-                  </Link></div>
+                  <Menu as="div" className="-ml-px relative block">
+        <Menu.Button className="border-transparent text-indigo-700  hover:text-indigo-400 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+          <span className="sr-only">Open options</span>
+          <div>Idioma</div>
+          <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="origin-top-right absolute right-0 mt-2 -mr-1 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {items.map((item) => (
+                <Menu.Item key={item.name}>
+                  {({ active }) => (
+                    <a
+                      href={item.href}
+                      className={classNames(
+                        active ? 'bg-gray-100 text-gray-500' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
                   
                 </div>
               </div>
